c66c7ca9b52feeea58ec7f8bee5aad29be210345
Sandro Benigno <sbenigno@gmail.com>
2020-10-08 19:54:11 -0300

    Adicionando cabeçalho e outras informações

--- HID_Ultrasonico_Diferencial.ino
+++ HID_Ultrasonico_Diferencial.ino
@@ -1,3 +1,24 @@
+/************************************************************************************
+ * Controle OSC com sensores LIDAR ou Ultrasônico                                   *
+ *                                                                                  *
+ * Este é um controle de ponteiro do Mouse no eixo X, utilizando um                 *
+ * Arduino Leonardo ou Pro Micro e dois sensores ultrassônicos de distância em modo *
+ * diferencial. Assim, tanto a direção quanto a velocidade do ponteiro são          *
+ * calculadas pela diferença da distância entre dois obstáculos, por exemplo        *
+ * duas mãos. Ou seja, se as duas mãos estiverem à mesma distância dos sensores,    *
+ * não há movimento. Caso haja uma diferença, o cursor se move para o mais próximo  *
+ * e quanto maior a diferença maior será a velocidade do ponteiro.                  *
+ *                                                                                  *
+ * Autor: Sandro Benigno                                                            *
+ * Licença de uso: GNU GPL 2.0                                                      *
+ *                                                                                  *
+ * The GNU GPL is the most widely used free software license and has a strong       *
+ * copyleft requirement. When distributing derived works, the source code of        *
+ * the work must be made available under the same license. There are multiple       *
+ * variants of the GNU GPL, each with different requirements.                       *
+ *                                                                                  *
+ **********************************************************************************/
+ 
 #include <Arduino.h>
 #include <Ultrasonic.h>
 #include <Mouse.h>
